c94a13a159b23e9e2b8d1cbcff67f8af29088690
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-04 23:08:41 -0600

    feat: adding firebase auth pt 1

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,16 +1,30 @@
 import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import { useAuthRequest } from "expo-auth-session/build/providers/Facebook";
 
 const AuthContext = createContext({
   // initial state of context (empty in this case)
 });
 
+// for prod, use env vars
+const config = {
+  androidClientId: process.env.ANDROID_CLIENT_ID,
+  iosClientId: process.env.IOS_CLIENT_ID,
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
+
 export const AuthProvider = ({ children }: any) => {
+  const signIn = async () => {
+    await Google.useAuthRequest();
+  };
+
   return (
     <AuthContext.Provider
       value={{
-        user: "Jacob",
-        // user: null,
+        // user: "Jacob",
+        user: null,
       }}
     >
       {children}
