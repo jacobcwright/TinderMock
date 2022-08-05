7459fb424482e6ae5382944209604533dd886902
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-04 21:48:18 -0600

    feat: added login page

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -6,6 +6,7 @@ import {
 } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 export default function StackNavigator() {
@@ -13,6 +14,7 @@ export default function StackNavigator() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 }
